99f034f0b22284b7ab63b23457de93b9d9f03ebf
Christian Kevin Kusuma <73664921+c-kevin-kusuma@users.noreply.github.com>
2022-07-17 20:23:21 -0600

    Add files via upload

--- R/contactListAll.R
+++ R/contactListAll.R
@@ -2,26 +2,23 @@
 #'
 #' This function allows you to quickly retrieve all contacts from 'HubSpot' via 'API' connection and output them as a dataframe.
 #' @param apiKey An 'API' key that can be generated from a 'HubSpot' instance.
-#' @param properties A vector of character values corresponds to 'HubSpot' interval names of the properties. Use 'contactProperties()' to check the internal names of the company properties. Defaults to 'c("firstname", "lastname", "email", "company")'.
-#' @examples contactListAll <- contactList(
-#'   apiKey = "demo",
-#'   properties = c("firstname", "lastname", "email", "company"))
+#' @examples contactListAll <- contactListAll(apiKey = "demo")
 #' @export
 
-contactListAll <- function(apiKey, properties = c("firstname", "lastname", "email", "company")) {
+contactListAll <- function(apiKey) {
   offset <- NA
   has_more <- TRUE
   contactListAll <- list()
 
   for (i in 1:9999999) {
     if (is.na(offset) == TRUE & has_more == TRUE) {
-      y <- companyList(apiKey = apiKey, properties = properties)
+      y <- rHubs::contactList(apiKey = apiKey)
       offset <- y$offset[1]
       has_more <- y$has_more[1]
       contactListAll[[i]] <- y
     }
     else if (is.na(offset) == FALSE & has_more == TRUE) {
-      y <- companyList(apiKey = apiKey, offset = offset, properties = properties)
+      y <- rHubs::contactList(apiKey = apiKey, offset = offset)
       offset <- y$offset[1]
       has_more <- y$has_more[1]
       contactListAll[[i]] <- y
@@ -32,7 +29,5 @@ contactListAll <- function(apiKey, properties = c("firstname", "lastname", "emai
   }
 
   contactListAll <- dplyr::bind_rows(contactListAll)
-
-
   return(contactListAll)
 }
